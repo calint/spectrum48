10bc27b9b5eb8ecd15031b27db64684049fb3b38
calint <calin.tenitchi@gmail.com>
2025-12-28 19:17:00 +0100

    .

--- src/main.asm
+++ src/main.asm
@@ -1,18 +1,26 @@
 org $8000
-  ld bc, MY_STRING
 
-MY_LOOP:
-  ld a, (bc)
-  cp 0
-  jr z, END_PROGRAM
-  rst $10
-  inc bc
-  jr MY_LOOP
+border: equ 5
 
-END_PROGRAM:
-  ret
+start:
+ld a, border
+out ($fe), a
 
-MY_STRING:
-  defb "Hello, world!"
-  defb 13, 0
-END $8000
+ld bc, hello
+
+.loop:
+ld a, (bc)
+cp 0
+jr z, done
+rst $10
+inc bc
+jr .loop
+
+done:
+ret
+
+hello:
+defb "hello, world!"
+defb 13, 0
+
+end start
