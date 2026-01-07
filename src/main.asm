8d0736e62fa70df1a87e268a4f1cb7195b37285e
calint <calin.tenitchi@gmail.com>
2026-01-07 16:17:39 +0100

    .

--- src/main.asm
+++ src/main.asm
@@ -7,6 +7,7 @@
 ; $5b00-$7fff : printer buffer / user ram
 ; $8000-$xxxx : code (this file)
 ; $fe00-$ff00 : im2 interrupt vector table (257 bytes)
+; $ff01-ffffe : stack
 ; $ffff       : interrupt handler (ret instruction)
 ;
 ; aligned data:
